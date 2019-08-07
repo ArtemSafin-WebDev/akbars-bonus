f60e66751cd4618bc500a7263c7af25fe0499fd0
Артем Сафин <artemsafin.webdev@gmail.com>
2019-08-07 19:05:51 +0300

    Внесены правки

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,13 +1,15 @@
-import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+import jump from 'jump.js';
 
 export default function() {
     const burgerButton = document.querySelector('.js-burger-menu-btn');
     const menuContent = document.querySelector('.burger-menu');
     const overlay = document.querySelector('.js-burger-overlay');
+    const burgerLinks = Array.from(document.querySelectorAll('.main-nav .main-nav__link'));
     let menuOpen = false;
     if (burgerButton && menuContent && overlay) {
         function handleBurgerMenu(event) {
-            event.preventDefault();
+            if (event) event.preventDefault();
             if (menuOpen) {
                 document.body.classList.remove('burger-menu-open');
                 menuOpen = false;
@@ -20,6 +22,24 @@ export default function() {
                 }
             }
         }
+
+        burgerLinks.forEach(link => {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                const elementToJump = document.querySelector(link.hash);
+                if (menuOpen) {
+                    handleBurgerMenu();
+                }
+                
+                if (!elementToJump) return;
+
+                jump(elementToJump, {
+                    duration: 1000,
+                    offset: -30,
+                    a11y: false
+                });
+            });
+        });
 
         burgerButton.addEventListener('click', handleBurgerMenu);
 
